8083777c974b4af56167f851699677be7def9118
Jakob Bregenhoj <nwptdane@gmail.com>
2026-03-25 15:03:11 +0100

    adding feature color method

---
+++ src/FEATURE_COLOR.py
@@ -0,0 +1,403 @@
+import cv2
+import numpy as np
+from math import sqrt, floor, ceil, nan, pi
+from skimage import color, exposure
+from skimage.color import rgb2gray
+from skimage.feature import blob_log
+from skimage.filters import threshold_otsu
+from skimage.measure import label, regionprops
+from skimage.transform import resize
+from skimage.transform import rotate
+from skimage import morphology
+from sklearn.cluster import KMeans
+from skimage.segmentation import slic
+from skimage.color import rgb2hsv
+from scipy.stats import circmean, circvar, circstd
+from statistics import variance, stdev
+from scipy.spatial import ConvexHull
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+data_path = 'C:/Users/nwptd/OneDrive/Desktop/DSdata/'
+
+def load_image_and_mask(image_id, data_path=data_path):
+    '''
+    Docstring for load_image
+    
+    :param image_id: "img_id" from metadata.csv
+    :param data_path: Relative path of the data folder
+
+    This functions takes as input an image ID, 
+    and returns the corresponding image and mask 
+    (found in "/data/imgs/" and "/data/masks/" respectively)
+    as an array
+    '''
+    img_path = data_path + "imgs/"
+    mask_path = data_path + "masks/"
+
+    file_im = img_path + image_id
+    file_mask = (mask_path + image_id).replace(".png", "_mask.png")
+
+    im = plt.imread(file_im)
+    mask = plt.imread(file_mask)
+
+    if im.shape[-1] == 4:
+        im = im[:, :, :3]
+
+    if len(mask.shape) == 3:
+        mask = rgb2gray(mask)
+
+    im = resize(im, (256, 256), anti_aliasing=True)
+    mask = resize(mask, (256, 256), anti_aliasing=True)
+
+    mask = mask > 0.5
+
+    return im, mask
+
+def cut_im_by_mask(image, mask):
+    masked = image.copy()
+    masked[mask == 0] = 0
+    return masked
+
+def get_multicolor_rate(im, mask, n):
+
+    im2 = im.copy()
+    im2[mask == 0] = 0
+
+    columns = im.shape[0]
+    rows = im.shape[1]
+    col_list = []
+    col_list = im2[mask != 0].reshape(-1, 3) * 256
+
+    if len(col_list) == 0:
+        return np.nan
+    
+    if len(col_list) < n:
+        return np.nan
+
+    cluster = KMeans(n_clusters=n, n_init=10).fit(col_list)
+    com_col_list = get_com_col(cluster, cluster.cluster_centers_)
+
+    dist_list = []
+    m = len(com_col_list)
+
+    if m <= 1:
+        return np.nan
+
+    for i in range(0, m - 1):
+        j = i + 1
+        col_1 = com_col_list[i]
+        col_2 = com_col_list[j]
+        dist_list.append(
+            np.sqrt(
+                (col_1[0] - col_2[0]) ** 2
+                + (col_1[1] - col_2[1]) ** 2
+                + (col_1[2] - col_2[2]) ** 2
+            )
+        )
+    return np.max(dist_list)
+
+def get_com_col(cluster, centroids):
+    com_col_list = []
+    labels = np.arange(0, len(np.unique(cluster.labels_)) + 1)
+    (hist, _) = np.histogram(cluster.labels_, bins=labels)
+    hist = hist.astype("float")
+    hist /= hist.sum()
+
+    rect = np.zeros((50, 300, 3), dtype=np.uint8)
+    colors = sorted([(percent, color) for (percent, color) in zip(hist, centroids)], key= lambda x:x[0])
+    start = 0
+    for percent, color in colors:
+        if percent > 0.08:
+            com_col_list.append(color)
+        end = start + (percent * 300)
+        cv2.rectangle(
+            rect,
+            (int(start), 0),
+            (int(end), 50),
+            color.astype("uint8").tolist(),
+            -1,
+        )
+        start = end
+    return com_col_list
+
+def slic_segmentation(image, mask, n_segments = 50, compactness = 0.1):
+    '''Get color segments of lesion from SLIC algorithm.
+    Optional argument n_segments (defualt 50) defines desired amount of segments.
+    Optional argument compactness (defualt 0.1) defines balance between color
+    and position.
+
+    Args:
+        image (numpy.ndarray): image to segment
+        mask (numpy.ndarray):  image mask
+        n_segments (int, optional): desired amount of segments
+        compactness (float, optional): compactness score, decides balance between
+            color and and position
+
+    Returns:
+        slic_segments (numpy.ndarray): SLIC color segments.
+    '''
+    slic_segments = slic(image,
+                    n_segments = n_segments,
+                    compactness = compactness,
+                    sigma = 1,
+                    mask = mask,
+                    start_label = 1,
+                    channel_axis = 2)
+
+    return slic_segments
+
+
+def get_rgb_means(image, slic_segments):
+    '''Get mean RGB values for each segment in a SLIC segmented image.
+
+    Args:
+        image (numpy.ndarray): original image
+        slic_segments (numpy.ndarray): SLIC segmentation
+
+    Returns:
+        rgb_means (list): RGB mean values for each segment.
+    '''
+
+    max_segment_id = np.unique(slic_segments)[-1]
+
+    rgb_means = []
+    for i in range(1, max_segment_id + 1):
+
+        #Create masked image where only specific segment is active
+        segment = image.copy()
+        segment[slic_segments != i] = -1
+
+        #Get average RGB values from segment
+        rgb_mean = np.mean(segment, axis = (0, 1), where = (segment != -1))
+
+        rgb_means.append(rgb_mean)
+
+    return rgb_means
+
+def get_hsv_means(image, slic_segments):
+    '''Get mean HSV values for each segment in a SLIC segmented image.
+
+    Args:
+        image (numpy.ndarray): original image
+        slic_segments (numpy.ndarray): SLIC segmentation
+
+    Returns:
+        hsv_means (list): HSV mean values for each segment.
+    '''
+
+    hsv_image = rgb2hsv(image)
+
+    max_segment_id = np.unique(slic_segments)[-1]
+
+    hsv_means = []
+    for i in range(1, max_segment_id + 1):
+
+        # Create masked image where only specific segment is active
+        segment = hsv_image.copy()
+        segment[slic_segments != i] = nan
+
+        #Get average HSV values from segment
+        hue_mean = circmean(segment[:, :, 0], high=1, low=0, nan_policy='omit') # Compute circular hue mean
+        sat_mean = np.mean(segment[:, :, 1], where = (slic_segments == i)) # Compute saturation mean
+        val_mean = np.mean(segment[:, :, 2], where = (slic_segments == i)) # Compute value mean
+
+        hsv_mean = np.asarray([hue_mean, sat_mean, val_mean])
+
+        hsv_means.append(hsv_mean)
+
+    return hsv_means
+
+def rgb_var(image, slic_segments):
+    '''Get variance of RGB means for each segment in
+    SLIC segmentation in red, green and blue channels
+
+    Args:
+        image (numpy.ndarray): image to compute color variance for
+        slic_segments (numpy.ndarray): array containing SLIC segmentation
+
+    Returns:
+        red_var (float): variance in red channel segment means
+        green_var (float): variance in green channel segment means
+        blue_var (float): variance in green channel segment means.
+    '''
+
+    # If there is only 1 slic segment, return (0, 0, 0)
+    if len(np.unique(slic_segments)) == 2: # Use 2 since slic_segments also has 0 for area outside mask
+        return 0, 0, 0
+
+    rgb_means = get_rgb_means(image, slic_segments)
+    n = len(rgb_means) # Amount of segments, used later to compute variance
+
+    # Seperate and collect channel means together in lists
+    red = []
+    green = []
+    blue = []
+    for rgb_mean in rgb_means:
+        red.append(rgb_mean[0])
+        green.append(rgb_mean[1])
+        blue.append(rgb_mean[2])
+
+    # Compute variance for each channel seperately
+    red_var = variance(red, sum(red)/n)
+    green_var = variance(green, sum(green)/n)
+    blue_var = variance(blue, sum(blue)/n)
+
+    return red_var, green_var, blue_var
+
+def hsv_var(image, slic_segments):
+    '''Get variance of HSV means for each segment in
+    SLIC segmentation in hue, saturation and value channels
+
+    Args:
+        image (numpy.ndarray): image to compute color variance for
+        slic_segments (numpy.ndarray): array containing SLIC segmentation
+
+    Returns:
+        hue_var (float): variance in hue channel segment means
+        sat_var (float): variance in saturation channel segment means
+        val_var (float): variance in value channel segment means.
+    '''
+
+    # If there is only 1 slic segment, return (0, 0, 0)
+    if len(np.unique(slic_segments)) == 2: # Use 2 since slic_segments also has 0 marking for area outside mask
+        return 0, 0, 0
+
+    hsv_means = get_hsv_means(image, slic_segments)
+    n = len(hsv_means) # Amount of segments, used later to compute variance
+
+    # Seperate and collect channel means together in lists
+    hue = []
+    sat = []
+    val = []
+    for hsv_mean in hsv_means:
+        hue.append(hsv_mean[0])
+        sat.append(hsv_mean[1])
+        val.append(hsv_mean[2])
+
+    # Compute variance for each channel seperately
+    hue_var = circvar(hue, high=1, low=0)
+    sat_var = variance(sat, sum(sat)/n)
+    val_var = variance(val, sum(val)/n)
+
+    return hue_var, sat_var, val_var
+
+
+def color_dominance(image, mask, clusters = 5, include_ratios = False):
+    '''Get the most dominent colors of the cut image that closest sorrounds the lesion using KMeans
+
+    Args:
+        image (numpy.ndarray): image to compute dominent colors for
+        mask (numpy.ndarray): mask of lesion
+        clusters (int, optional): amound of clusters and therefore dominent colors (defualt 3)
+        include_ratios (bool, optional): whether to include the domination ratios for each color (defualt False)
+
+    Return:
+        if include_ratios == True:
+            p_and_c (list): list of tuples, each containing the percentage and RGB array of the dominent color
+        else:
+            dom_colors (array): array of RGB arrays of each dominent color.
+    '''
+
+    # Prepare image for KMeans
+    cut_im = cut_im_by_mask(image, mask) # Cut image to remove excess skin pixels
+    hsv_im = rgb2hsv(cut_im) # Convert image to HSV color space
+    flat_im = np.reshape(hsv_im, (-1, 3)) # Flatten image to 2D array
+
+    # Use KMeans to cluster image by colors
+    k_means = KMeans(n_clusters=clusters, n_init=10, random_state=0)
+    k_means.fit(flat_im)
+
+    # Save cluster centers (dominant colors) in array
+    dom_colors = np.array(k_means.cluster_centers_, dtype='float32')
+
+    if include_ratios:
+
+        counts = np.unique(k_means.labels_, return_counts=True)[1] # Get count of each dominent color
+        ratios = counts / flat_im.shape[0] # Get percentage of total image for each dominent color
+
+        r_and_c = zip(ratios, dom_colors) # Percentage and colors
+        r_and_c = sorted(r_and_c, key=lambda x: x[0],reverse=True) # Sort in descending order
+
+        return r_and_c
+
+    return dom_colors
+
+
+
+def get_relative_rgb_means(image, slic_segments):
+    '''Get mean RGB values for each segment in a SLIC segmented image.
+
+    Args:
+        image (numpy.ndarray): original image
+        slic_segments (numpy.ndarray): SLIC segmentation
+
+    Returns:
+        rgb_means (list): RGB mean values for each segment.
+    '''
+
+    max_segment_id = np.unique(slic_segments)[-1]
+
+    rgb_means = []
+    for i in range(0, max_segment_id + 1):
+
+        #Create masked image where only specific segment is active
+        segment = image.copy()
+        segment[slic_segments != i] = -1
+
+        #Get average RGB values from segment
+        rgb_mean = np.mean(segment, axis = (0, 1), where = (segment != -1))
+
+        rgb_means.append(rgb_mean)
+
+    rgb_means_lesion = np.mean(rgb_means[1:],axis=0)
+    rgb_means_skin = np.mean(rgb_means[0])
+
+    F1, F2, F3 = rgb_means_lesion/sum(rgb_means_lesion)
+    F10, F11, F12 = rgb_means_lesion - rgb_means_skin
+
+    return F1, F2, F3, F10, F11, F12
+
+
+def extract_features(row, data_path=data_path):
+    img_id = row["img_id"]
+    im, mask = load_image_and_mask(img_id, data_path)
+
+    if np.sum(mask) == 0:
+        return {"diagnostic": row["diagnostic"]}
+
+    slic_segments = slic_segmentation(im, mask)
+
+    red_var, green_var, blue_var = rgb_var(im, slic_segments)
+    hue_var, sat_var, val_var = hsv_var(im, slic_segments)
+
+    feats = {
+        "diagnostic": row["diagnostic"],
+        "rgb_red_var": red_var,
+        "rgb_green_var": green_var,
+        "rgb_blue_var": blue_var,
+        "hue_var": hue_var,
+        "sat_var": sat_var,
+        "val_var": val_var,
+    }
+
+    return feats
+
+df = pd.read_csv("../data/metadata.csv")
+df = df.head(100)
+
+# Apply feature extraction to each row
+features_list = []
+
+for i, (_, row) in enumerate(df.iterrows()):
+    print(f"Processing {i+1}/{len(df)}: {row['img_id']}")
+    feats = extract_features(row)
+    features_list.append(feats)
+
+# Convert to DataFrame
+features_df = pd.DataFrame(features_list)
+
+# Save results
+#features_df.to_csv("../data/features.csv", index=False)
+
