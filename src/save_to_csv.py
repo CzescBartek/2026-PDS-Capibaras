edf9b2515acb523dc0e47b1a2f04ebaa13e335c9
Krzysztof Marcinkiewicz <marcinkiewicz90@gmail.com>
2026-04-15 13:10:03 +0200

    Finished KNN.py File

--- src/save_to_csv.py
+++ src/save_to_csv.py
@@ -6,6 +6,9 @@ from FEATURE_A import asymmetry, midpointGroup9
 import os
 from FEATURE_COLOR import slic_segmentation, get_rgb_means, load_image_and_mask
 from FEATURE_BORDER import compactness_score, convexity_score
+from hair_coverage import hair_coverage
+from hair_remove import removeHair_auto
+from penmark_remove import remove_penmarks
 import cv2
 import numpy as np
 from math import sqrt, floor, ceil, nan, pi
@@ -20,8 +23,8 @@ from skimage.color import rgb2hsv
 from statistics import variance, stdev
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
 import os
+import cv2
 
 df = pd.read_csv("../data/metadata.csv")
 image_id = df["img_id"].tolist()
@@ -45,7 +48,7 @@ b = []
 compactness = []
 convexity = []
 cancerous = []
-
+hair = []
 features = []
 for files in image_id:
     value = df.loc[df['img_id'] == files, 'diagnostic'].values
@@ -54,22 +57,34 @@ for files in image_id:
         cancerous.append(1)
     else:
         cancerous.append(0)
-
+    #This for color
     im, mask = load_image_and_mask(files, data_path=data_path)
-    features.append(asymmetry(mask))
+
+    path = '../data/imgs/' + files
+    img_org = cv2.imread(path)
+    img_gray = cv2.cvtColor(img_org, cv2.COLOR_BGR2GRAY)
+    img1 = remove_penmarks(img_org)
+    img_gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img_out = removeHair_auto(img1, img_gray1)
+    im = cv2.cvtColor(img_out, cv2.COLOR_BGR2RGB)
+    if im.shape[:2] != mask.shape[:2]:
+        mask_uint8 = mask.astype(np.uint8) * 255
+        mask = cv2.resize(mask_uint8, (im.shape[1], im.shape[0]), interpolation=cv2.INTER_NEAREST)
+
     means = get_rgb_means(im, mask)
     if means is not None and len(means) > 0:
         colors = np.mean(means, axis=0)
     else:
         colors = np.array([0, 0, 0])
 
-    img_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    features.append(asymmetry(mask))
     compactness.append(compactness_score(mask))
     convexity.append(convexity_score(mask))
     r.append(colors[0])
     g.append(colors[1])
     b.append(colors[2])
-    print(im)
+    hair.append(hair_coverage(img_gray))
+    print(files)
 
 
 df_features['FEATURE_A'] = features
@@ -82,6 +97,8 @@ df_features['FEATURE_BORDER_COMPACTNESS'] = compactness
 df_features['FEATURE_BORDER_CONVEXITY'] = convexity
 
 df_features['Cancerous'] = cancerous
+
+df_features['Hair'] = hair
 
 df_features.to_csv(csv_path, index=False)
 
