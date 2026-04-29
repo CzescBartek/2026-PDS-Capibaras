b9e300f10b996f093141c994056b53b2794894aa
Jakob Bregenhoj <nwptdane@gmail.com>
2026-04-29 14:45:45 +0200

    updated feature color to use hsv values instead of rgb

--- src/FEATURE_COLOR.py
+++ src/FEATURE_COLOR.py
@@ -78,34 +78,38 @@ def slic_segmentation(image, mask, n_segments = 50, compactness = 0.1):
     return slic_segments
 
 
-def get_rgb_means(image, mask):
-    '''Get mean RGB values for each segment in a SLIC segmented image.
+def get_hsv_means(image, slic_segments):
+    '''Get mean HSV values for each segment in a SLIC segmented image.
 
     Args:
         image (numpy.ndarray): original image
         slic_segments (numpy.ndarray): SLIC segmentation
 
     Returns:
-        rgb_means (list): RGB mean values for each segment.
+        hsv_means (list): HSV mean values for each segment.
     '''
-
     if image.size == 0 or np.sum(mask) == 0:
         return None
-    slic_segments = slic_segmentation(image, mask)
+
+    hsv_image = rgb2hsv(image)
 
     max_segment_id = np.unique(slic_segments)[-1]
 
-    rgb_means = []
+    hsv_means = []
     for i in range(1, max_segment_id + 1):
 
-        #Create masked image where only specific segment is active
-        segment = image.copy().astype(np.int16)
-        segment[slic_segments != i] = -1
+        # Create masked image where only specific segment is active
+        segment = hsv_image.copy()
+        segment[slic_segments != i] = nan
 
-        #Get average RGB values from segment
-        rgb_mean = np.mean(segment, axis = (0, 1), where = (segment != -1))
+        #Get average HSV values from segment
+        hue_mean = circmean(segment[:, :, 0], high=1, low=0, nan_policy='omit') # Compute circular hue mean
+        sat_mean = np.mean(segment[:, :, 1], where = (slic_segments == i)) # Compute saturation mean
+        val_mean = np.mean(segment[:, :, 2], where = (slic_segments == i)) # Compute value mean
 
-        rgb_means.append(rgb_mean)
+        hsv_mean = np.asarray([hue_mean, sat_mean, val_mean])
 
-    return rgb_means
+        hsv_means.append(hsv_mean)
+
+    return hsv_means
 
